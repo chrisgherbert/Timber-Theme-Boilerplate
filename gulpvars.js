c7515d3e61a8ed2f2fb6fc57c44c745679c94fa0
Chris Herbert <herbert@bermanco.com>
2019-11-11 20:59:05 -0500

    Initial commit

---
+++ gulpvars.js
@@ -0,0 +1,17 @@
+module.exports = {
+
+  jsFiles: [
+    'assets/bower_components/jquery-smooth-scroll/jquery.smooth-scroll.js',
+    'assets/bower_components/lity/dist/lity.js',
+    'assets/js/modernizr.js',
+    'assets/js/main.js',
+    'assets/js/**/*.js'
+  ],
+
+  scssPaths: [
+    'assets/bower_components/bootstrap-sass/assets/stylesheets/',
+    'assets/bower_components/font-awesome/scss/',
+    'assets/scss/**/*.scss'
+  ]
+
+};
