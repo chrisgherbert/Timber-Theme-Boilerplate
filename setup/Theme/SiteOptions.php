ef218d37944f16ea63d6d15c6737287c4f5532c6
Chris Herbert <herbert@bermanco.com>
2020-04-11 22:03:54 -0400

    Removed repetitive code in site options setup

--- setup/Theme/SiteOptions.php
+++ setup/Theme/SiteOptions.php
@@ -34,6 +34,82 @@ class SiteOptions {
 
 	}
 
+	public function add_options_page($slug, $title, $tab_title = null){
+
+		$args = [
+			'id'           => $this->options_key . '_page_' .$slug,
+			'title'        => $title,
+			'menu_title'   => $title, // Use menu title, & not title to hide main h2.
+			'object_types' => [ 'options-page' ],
+			'option_key'   => $this->options_key . '_' . $slug,
+			'parent_slug'  => $this->options_key,
+			'tab_group'    => $this->options_key,
+			'tab_title'    => $tab_title ?? $title,
+		];
+
+		$cmb = new_cmb2_box($args);
+
+		return $cmb;
+
+	}
+
+	public function tracking_options_page(){
+
+		$options_page = $this->add_options_page(
+			'tracking',
+			'Tracking Options',
+			'Tracking'
+		);
+
+		$options_page->add_field( [
+			'name'    => 'After opening &lt;head&gt;',
+			'desc'    => 'This code will be output immediately after the opening &lt;head&gt; tag.',
+			'id'      => 'after_opening_head',
+			'type'    => 'textarea_code',
+		] );
+
+		$options_page->add_field( [
+			'name'    => 'Before closing &lt;/head&gt;',
+			'desc'    => 'This code will be output immediately before the closing &lt;/head&gt; tag.',
+			'id'      => 'before_closing_head',
+			'type'    => 'textarea_code',
+		] );
+
+		$options_page->add_field( [
+			'name'    => 'After opening &lt;body&gt;',
+			'desc'    => 'This code will be output immediately after the opening &lt;body&gt; tag.',
+			'id'      => 'after_opening_body',
+			'type'    => 'textarea_code',
+		] );
+
+		$options_page->add_field( [
+			'name'    => 'Before closing &lt;/body&gt;',
+			'desc'    => 'This code will be output immediately before the closing &lt;/body&gt; tag.',
+			'id'      => 'before_closing_body',
+			'type'    => 'textarea_code',
+		] );
+
+	}
+
+	public function footer_options_page(){
+
+		$options_page = $this->add_options_page(
+			'footer',
+			'Footer Options',
+			'Footer'
+		);
+
+		$options_page->add_field( [
+			'name'    => 'Footer Content',
+			'id'      => 'footer_content',
+			'type'    => 'wysiwyg',
+			'options' => [
+				'textarea_rows' => 5
+			]
+		] );
+
+	}
+
 	public function social_options_main_page(){
 
 		/**
@@ -100,87 +176,6 @@ class SiteOptions {
 			'name' => 'YouTube Page URL',
 			'id'   => 'youtube_url',
 			'type' => 'text_url',
-		] );
-
-	}
-
-	public function tracking_options_page(){
-
-		/**
-		 * Registers secondary options page, and set main item as parent.
-		 */
-		$suffix = '_tracking';
-
-		$args = [
-			'id'           => $this->options_key . '_page' . $suffix,
-			'title'        => 'Tracking Options',
-			'menu_title'   => 'Tracking Options', // Use menu title, & not title to hide main h2.
-			'object_types' => [ 'options-page' ],
-			'option_key'   => $this->options_key . $suffix,
-			'parent_slug'  => $this->options_key,
-			'tab_group'    => $this->options_key,
-			'tab_title'    => 'Tracking',
-		];
-
-		$tracking_options = new_cmb2_box( $args );
-
-		$tracking_options->add_field( [
-			'name'    => 'After opening &lt;head&gt;',
-			'desc'    => 'This code will be output immediately after the opening &lt;head&gt; tag.',
-			'id'      => 'after_opening_head',
-			'type'    => 'textarea_code',
-		] );
-
-		$tracking_options->add_field( [
-			'name'    => 'Before closing &lt;/head&gt;',
-			'desc'    => 'This code will be output immediately before the closing &lt;/head&gt; tag.',
-			'id'      => 'before_closing_head',
-			'type'    => 'textarea_code',
-		] );
-
-		$tracking_options->add_field( [
-			'name'    => 'After opening &lt;body&gt;',
-			'desc'    => 'This code will be output immediately after the opening &lt;body&gt; tag.',
-			'id'      => 'after_opening_body',
-			'type'    => 'textarea_code',
-		] );
-
-		$tracking_options->add_field( [
-			'name'    => 'Before closing &lt;/body&gt;',
-			'desc'    => 'This code will be output immediately before the closing &lt;/body&gt; tag.',
-			'id'      => 'before_closing_body',
-			'type'    => 'textarea_code',
-		] );
-
-	}
-
-	public function footer_options_page(){
-
-		/**
-		 * Registers footer options page, and set main item as parent.
-		 */
-		$suffix = '_footer';
-
-		$args = [
-			'id'           => $this->options_key . '_page' . $suffix,
-			'title'        => 'Footer Options',
-			'menu_title'   => 'Footer Options', // Use menu title, & not title to hide main h2.
-			'object_types' => [ 'options-page' ],
-			'option_key'   => $this->options_key . $suffix,
-			'parent_slug'  => $this->options_key,
-			'tab_group'    => $this->options_key,
-			'tab_title'    => 'Footer',
-		];
-
-		$tracking_options = new_cmb2_box( $args );
-
-		$tracking_options->add_field( [
-			'name'    => 'Footer Content',
-			'id'      => 'footer_content',
-			'type'    => 'wysiwyg',
-			'options' => [
-				'textarea_rows' => 5
-			]
 		] );
 
 	}
