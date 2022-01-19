174deb5443e9df3070268afe4fda303dd88c9c20
chrisgherbert <chris@chrisgherbert.com>
2022-01-18 21:19:06 -0500

    Began work on csv importer for ballotpedia race data

---
+++ setup/Content/Import/RaceRecord.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Content\Import;
+
+class RaceRecord {
+
+	public $data;
+
+	public function __construct($row){
+
+		$this->data = $row;
+
+	}
+
+	public function create_post(){
+
+		// do stuff
+
+	}
+
+}
---
+++ setup/Content/Import/RacesBallotpediaImporter.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Content\Import;
+
+use League\Csv\Reader;
+
+class RacesBallotpediaImporter {
+
+	public $csv_path;
+
+	public function __construct($csv_path){
+
+		$this->csv_path = $csv_path;
+
+		if (!file_exists($this->csv_path)){
+			return 'Invalid path';
+		}
+
+	}
+
+	public function import(){
+
+		$csv = Reader::createFromPath($this->csv_path, 'r');
+
+		$csv->setHeaderOffset(0);
+
+		// Get records and convert to array
+		$iterator = $csv->getRecords();
+		$records = iterator_to_array($iterator);
+
+		foreach ($records as $row){
+
+			$race_record = new RaceRecord($row);
+
+			$race_record->create_post();
+
+		}
+
+	}
+
+}
