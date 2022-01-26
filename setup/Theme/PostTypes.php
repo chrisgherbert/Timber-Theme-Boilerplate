9b35e07854782a42c6a7ba63ca0e1fdf54fa9816
chrisgherbert <chris@chrisgherbert.com>
2022-01-26 01:19:20 -0500

    Removed sample code

--- setup/Theme/PostTypes.php
+++ setup/Theme/PostTypes.php
@@ -20,30 +20,6 @@ class PostTypes {
 		}
 	}
 
-	/**
-	 * Sample "article" content type - used in place of the default "post" content type
-	 * to allow a custom rewrite base.  This makes it easier to track blog/article traffic
-	 * specifically in analytics software.  However, it also can complicate the development
-	 * process.  Proceed with caution.
-	 */
-	public function article(){
-
-		register_via_cpt_core(
-			[
-				'Article', // Singular Name
-				'Articles', // Plural Name
-				'article' // Post Type Slug
-			],
-			[
-				'menu_icon' => 'dashicons-admin-page', // Dashicon icon (Reference: https://developer.wordpress.org/resource/dashicons/)
-				'supports' => ['title', 'editor', 'thumbnail', 'excerpt'],
-				'taxonomies' => ['category', 'post_tag'],
-				'has_archive' => true
-			]
-		);
-
-	}
-
 	public function candidate(){
 
 		register_via_cpt_core([
