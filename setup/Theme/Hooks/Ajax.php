c53f02f99e5d00f796315ad9fde1759ced806a05
chrisgherbert <chris@chrisgherbert.com>
2022-01-26 12:19:54 -0500

    Added ajax action for updating Ballotpedia data.
    Easier to deal with Cloudways cron jobs with a URL rather than running a script directly

--- setup/Theme/Hooks/Ajax.php
+++ setup/Theme/Hooks/Ajax.php
@@ -6,6 +6,16 @@ class Ajax {
 
 	public function __construct(){
 
+		add_action('wp_ajax_update_ballotpedia_data', [$this, 'update_ballotpedia_data']);
+		add_action('wp_ajax_nopriv_update_ballotpedia_data', [$this, 'update_ballotpedia_data']);
+
+	}
+
+	public function update_ballotpedia_data(){
+
+		// Update Ballotpedia data and send report email
+		import_ballotpedia_data(true);
+
 	}
 
 }
